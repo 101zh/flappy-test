179d235aec2aa5278cdf49483ec63b83e04aceb1
101zh <67253838+101zh@users.noreply.github.com>
2024-06-26 16:41:31 -0700

    fix retry screen retriggering

--- scripts/bird.gd
+++ scripts/bird.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 
 const JUMP_VELOCITY = -150.0
 const GRAVITY = 650
-var isMovementDisabled : bool = false
+var dead : bool = false
 
 func _physics_process(delta):
 	
@@ -13,21 +13,17 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 
-	
 	# Handle jump.
-	if !isMovementDisabled && Input.is_action_just_pressed("ui_accept"):
+	if !dead && Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
 	
 	if move_and_slide():
 		die()
 
-
-func disable_movement():
-	isMovementDisabled = true
-	
 func die():
-
-	disable_movement()
+	if dead:
+		return
+	dead = true
 	game_manager.enable_retry_screen()
 	
 	
