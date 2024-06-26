61b84a1fca69ecd9f4bac4da5658f6b9d217cf67
101zh <67253838+101zh@users.noreply.github.com>
2024-06-26 16:59:56 -0700

    add a score counter

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -7,21 +7,22 @@ var pipe = preload( "res://scenes/pipe_section.tscn")
 @onready var retry_screen = $"../CanvasLayer/RetryScreen"
 @onready var bird = $"../bird"
 @onready var timer = $Timer
+@onready var score_label = %ScoreLabel
 
 var topLeft : Vector2
 var bottomRight : Vector2
-
 
 func _ready():
 	var canvas = get_canvas_transform()
 	var size = get_viewport_rect().size / canvas.get_scale()
 	topLeft = -canvas.origin / canvas.get_scale()
 	bottomRight = Vector2(topLeft.x+size.x, topLeft.y+size.y)
+	
 	spawn_pipes()
 
 func add_score():
-	score+=1
-	print(score)
+	score += 1
+	score_label.text = "Score: "+ str(score)
 	
 func enable_retry_screen():
 	retry_screen.set_process(true)
