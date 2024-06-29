ce6b13506eeb851e5667cd46ec16f262479d1257
101zh <67253838+101zh@users.noreply.github.com>
2024-06-28 17:55:25 -0700

    add more buttons to retry screen
    - buttons: main-menu, options, & quit
    - also made retry screen separate scene

--- scripts/RetryScreen.gd
+++ scripts/RetryScreen.gd
@@ -1,12 +1,27 @@
 extends Control
 
+@onready var retry = $"Button Container/Retry"
+
 func _ready():
 	self.set_process(false)
 	self.visible = false
 
 func enable():
+	retry.grab_focus()
 	var animation_player = $AnimationPlayer
 	animation_player.play("fade_in")
 
-func _on_button_pressed():
+func _on_retry_pressed():
 	get_tree().reload_current_scene()
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://scenes/startMenu.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
