ecff1808ed270c1429510c97e6aedfbe022a6806
101zh <67253838+101zh@users.noreply.github.com>
2024-06-28 17:44:17 -0700

    fix pipe position randomization

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var score : int = 0
-var pipe_spawn_time : float = 1.5
+var pipe_spawn_time : float = 1.25
 var pipe = preload( "res://scenes/pipe_section.tscn")
 @onready var animation_player = $"../CanvasLayer/RetryScreen/AnimationPlayer"
 @onready var retry_screen = $"../CanvasLayer/RetryScreen"
@@ -17,7 +17,6 @@ func _ready():
 	var size = get_viewport_rect().size / canvas.get_scale()
 	topLeft = -canvas.origin / canvas.get_scale()
 	bottomRight = Vector2(topLeft.x+size.x, topLeft.y+size.y)
-	
 	spawn_pipes()
 
 func add_score():
--- scripts/pipe_section.gd
+++ scripts/pipe_section.gd
@@ -6,12 +6,15 @@ extends Node2D
 @onready var game_manager = $"../%GameManager"
 
 var speed : float = 100
-var minOpening : float = 18.0
-var maxOpening : float = 30.0
+var minOpening : float = 17.0
+var maxOpening : float = 28.0
 var topLeft
-var randomRangeMidLine=1
+var opening : float
+var midLineRange : float
 
 func _ready():
+	opening = randf_range(minOpening, maxOpening)
+	midLineRange = (game_manager.bottomRight.y-topLeft.y) - opening - 25
 	randomize_pos()
 
 func _process(delta):
@@ -21,15 +24,14 @@ func _process(delta):
 		transform = transform.translated(Vector2(-speed * delta, 0))
 
 func randomize_pos():
-	var midY = (randf()*randomRangeMidLine)-(randomRangeMidLine/2)
-	var distanceY = randf_range(minOpening, maxOpening)
-	top_pipe.move_local_y(midY-distanceY) 
-	bottom_pipe.move_local_y(midY+distanceY)
+	var midY = (randf()*midLineRange)-(midLineRange/2)
+	top_pipe.move_local_y(midY-opening) 
+	bottom_pipe.move_local_y(midY+opening)
 
 func initialize(instance):
 	game_manager = instance
 	topLeft = game_manager.topLeft
-	randomRangeMidLine = (game_manager.bottomRight.y-topLeft.y)-(maxOpening*3)
+	midLineRange = (game_manager.bottomRight.y-topLeft.y)-(maxOpening*3)
 
 func _on_point_trigger_body_entered(body):
 	game_manager.add_score()  
